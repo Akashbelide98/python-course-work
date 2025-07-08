e201618c026ea888a6993d995322513166526855
Akash <akashbelide98@gmail.com>
2025-07-08 14:32:30 +0530

    demo

---
+++ Classwork/4. pythonOperators.py
@@ -0,0 +1,19 @@
+'''#1. Arthemetic Operators
+a = 20
+b = 30
+print('Addition(a+b):',a+b) #Addition(a+b): 50
+print('subtraction(a-b):',a-b) #subtraction(a-b): -10
+print('multiplication(a*b):',a*b)#multiplication(a*b): 600
+print('division(a/b):',a/b) #division(a/b): 0.6666666666666666
+print('Modulus(a%b):',a%b) #Modulus(a%b): 20
+print('Expoential(a**b):',a**b) #Expoential(a**b): 1073741824000000000000000000000000000000'''
+
+#2.Comparisom Operators
+c=56
+d=26
+print('Equal to(c==d):',c==d) #Equal to(c==d): False
+print('Not Equal to(c!=d):',c!=d) #Not Equal to(c!=d): True
+print('Greater than(c>d):',c>d) #Greater than(c>d): True
+print('Less than(c<d):',c<d) #Less than(c<d): False
+print('Greater than or equal(c>=d)',c>=d) #Greater than or equal(c>=d) True
+print('Less than or equal to(c<=d)',c<=d) #Less than or equal to(c<=d) False
