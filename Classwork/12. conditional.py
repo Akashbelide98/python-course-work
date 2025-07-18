f536620b2ab65b32fefdb7fa296a59e7f716f11b
Akash <akashbelide98@gmail.com>
2025-07-18 13:42:00 +0530

    Initial commit

---
+++ Classwork/12. conditional.py
@@ -0,0 +1,17 @@
+#conditionals stmts
+#1.simple if statement
+'''hr,mins = list(map(int,input("enter the HH:MM : ").split(':')))
+
+if hr>=0 and hr<=24 and mins>0 and mins<=60:
+    if hr>=0 and hr<4:
+        print("Its high timr,Time to sleep")
+    elif hr>=4 and hr<12:
+        print("Good Morning. Hve a nice day")
+    elif hr>=12 and hr<16:
+        print('Good Afternoon. Have a great Lunch')
+    elif hr>=16 and hr<20:
+        print('Good Evening. Have some snacks')
+    elif hr>=20 and hr<24:
+        print('Good Night.Sweet Dreams')
+else:
+    print('Enter the proper input,your input is invalid')'''
---
+++ Classwork/7.2 Dict.py
@@ -0,0 +1,6 @@
+'''d={}
+d=dict()
+print(d)
+print(type(d))
+d={int:35,"String":"hello",float:3.14,bool:True,list:[1,2,3,4,5],tuple:(1,2,3,4,5),dict:{"a":1,"b":2,"c":3}}  # Checking the type of d
+print (d)'''
